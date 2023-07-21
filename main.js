7668ab91799b948fbf5667bf5f485e4e9ce0b79d
Indika Fernando <Indika.Fernando@taxsystems.com>
2023-07-21 11:15:54 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './dotnet.js'
+
+const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+    .withDiagnosticTracing(false)
+    .withStartupOptions({
+        loadBootResource: function (type, name, defaultUri, integrity) {
+            // Override extension of ICU files
+            if (type == 'globalization') {
+                defaultUri = defaultUri.replace('.dat', '.icu');
+            }
+
+            return defaultUri;
+        }
+    })
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+setModuleImports('main.js', {
+    window: {
+        location: {
+            href: () => globalThis.window.location.href
+        }
+    }
+});
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+const text = exports.MyClass.Greeting();
+console.log(text);
+
+document.getElementById('out').innerHTML = text;
+await dotnet.run();
